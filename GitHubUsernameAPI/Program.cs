8c8722009ceb882745152bff4ab440e16505e08d
Ian Bittencourt Andrade <ianbandrade14@gmail.com>
2020-12-22 12:00:45 -0300

    first commit

---
+++ GitHubUsernameAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestEase;
+
+namespace GitHubAPI
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Blog { get; set; }
+        [JsonProperty("created_at")] public DateTime CreatedAt { get; set; }
+    }
+
+    [Header("User-Agent", "RestEase")]
+    public interface IGitHubApi
+    {
+        [Get("users/{userId}")]
+        Task<User> GetUserAsync([Path] string userId);
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IGitHubApi api = RestClient.For<IGitHubApi>("https://api.github.com");
+
+            Console.WriteLine("Type your GitHub username: ");
+            string userName = Console.ReadLine();
+
+            User user = api.GetUserAsync(userName).Result;
+            Console.WriteLine($"Name: {user.Name}. Blog: {user.Blog}. CreatedAt: {user.CreatedAt}");
+            Console.ReadLine();
+        }
+    }
+}
